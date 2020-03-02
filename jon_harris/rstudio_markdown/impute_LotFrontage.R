52cb81d0c0d8a27c0c3e9b5cb31b1f9db1ad2ac5
JonHarris <jonathan.harris.eit@gmail.com>
2020-03-01 23:45:29 -0500

    completed xgboost and ppt draft

--- jon_harris/rstudio_markdown/impute_LotFrontage.R
+++ jon_harris/rstudio_markdown/impute_LotFrontage.R
@@ -13,3 +13,31 @@ hp_orig$LotFrontage = impute_LotFrontage
 save(hp_feat, hp_orig, file = "train_all_v2.RData")
 
 
+
+
+
+
+#IMPUTE TEST DATA: LOTFRONTAGE
+load('test_all.RData') #load test data
+original_test = read.csv2('test.csv', header=T, sep = ',')
+impute_LotFrontage = original_test$LotFrontage
+for (neigh in original_test$Neighborhood){
+  idx = which(is.na(original_test$LotFrontage) & (original_test$Neighborhood==neigh))
+  impute_LotFrontage[idx] =  mean(original_test$LotFrontage[original_test$Neighborhood==neigh], na.rm=T)
+}
+hp_feat_test$LotFrontage = impute_LotFrontage
+hp_orig_test$LotFrontage = impute_LotFrontage
+
+#IMPUTE/FEAT_ENGINEER TEST DATA: TOTGARAGECARS
+idx = which(is.na(original_test$GarageCars))
+original_test$GarageCars[idx] = 0
+idx = which(original_test$GarageCars >= 3)
+original_test$GarageCars[idx] = 3
+hp_feat_test$TotCarGarage = original_test$GarageCars
+
+hp_orig_test$Exterior1st[692] = 11
+hp_orig_test$Exterior2nd[692] = 14
+
+save(hp_feat_test, hp_orig_test, file = "test_all_v2.RData")
+
+
